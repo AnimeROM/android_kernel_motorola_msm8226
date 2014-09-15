1d0a20bec44f4ae76e5b10633362ea79a00798ad
klozz <xxx.reptar.rawrr.xxx@gmail.com>
2014-09-15 13:22:23 -0500

    fix shitt build from moto
    Signed-off-by: klozz <xxx.reptar.rawrr.xxx@gmail.com>

--- sound/soc/msm/msm8226.c
+++ sound/soc/msm/msm8226.c
@@ -26,7 +26,7 @@
 #include <sound/q6afe-v2.h>
 #include <mach/socinfo.h>
 #include <mach/subsystem_notif.h>
-#include <qdsp6v2/msm-pcm-routing-v2.h>
+#include "qdsp6v2/msm-pcm-routing-v2.h"
 #include "qdsp6v2/q6core.h"
 #include "../codecs/wcd9xxx-common.h"
 #include "../codecs/wcd9306.h"
